da5814db599eb8b789c64bc2c665c239b95af3ea
Luis Reyes <luisreyesxv@yahoo.com>
2020-07-14 01:34:58 -0400

    done

--- app.rb
+++ app.rb
@@ -2,8 +2,13 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! "
+  get '/' do
+    "Started my server using Shotgun!"
   end
+
+  get '/luis/' do
+    "i'm am amazing. i may start to understand sinatra"
+  end
+
 
 end
